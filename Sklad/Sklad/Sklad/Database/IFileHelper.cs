712385aececf09b40e3198f8dca1dc88c963db7a
Jan Kočvara <kocvaja14@sps-prosek.cz>
2017-06-14 20:14:21 +0200

    v0.1.1 -> vytvoření databáze

---
+++ Sklad/Sklad/Sklad/Database/IFileHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace Sklad
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
---
+++ Sklad/Sklad/Sklad/Database/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Sklad.Database
+{
+    public class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Name { get; set; }
+        public string Map { get; set; }
+        public DateTime Cas { get; set; }
+        public bool Ucast { get; set; }
+        public string Description { get; set; }
+        public Item()
+        {
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + " Map: " + Map + " Čas: " + Cas;
+        }
+    }
+
+}
